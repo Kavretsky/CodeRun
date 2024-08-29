3481e041d08877a536d2277cbdc901aa51557766
Nikolay Kavretsky <kavretsky@gmail.com>
2024-08-29 16:13:42 +0300

    solve task 1

---
+++ CodeRun/easy/LowBattery.swift
@@ -0,0 +1,17 @@
+//
+//  LowBattery.swift
+//  CodeRun
+//
+//  Created by Nikolay Kavretsky on 29.08.2024.
+//
+
+import Foundation
+
+//1 Села батарейка
+
+func lowBattery() {
+    let appNum = Int(readLine()!)
+    let appUsage = readLine()!.split(separator: " ").map { Int($0)! }.reduce(0, +)
+    
+    print(100 / appUsage)
+}
