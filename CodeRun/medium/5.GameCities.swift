3b7d01e68029ce40079d1f07c819d8bd84eed8c7
Nikolay Kavretsky <kavretsky@gmail.com>
2024-08-29 18:45:12 +0300

    solve task 5

--- CodeRun/medium/5.GameCities.swift
+++ CodeRun/medium/5.GameCities.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+//5 Игра в города
+
+func gameCities() {
+    var input1 = readLine()!
+    if let dotIndex = input1.firstIndex(of: ".") {
+        input1.remove(at: dotIndex)
+    }
+    var array1 = input1.split(separator: ", ").map { String($0) }
+    var input2 = readLine()!
+    if let dotIndex = input2.firstIndex(of: ".") {
+        input2.remove(at: dotIndex)
+    }
+    var array2 = input2.split(separator: ", ").map { String($0) }
+    var set1 = Set<Set<String>>()
+    
+    for city in array1 {
+        let cityAndCountry = city.split(separator: " ").map{ String($0) }
+        let set: Set<String> = [cityAndCountry[0], cityAndCountry[1]]
+        set1.insert(set)
+    }
+    
+    for city in array2 {
+        let cityAndCountry = city.split(separator: " ").map{ String($0) }
+        let set: Set<String> = [cityAndCountry[0], cityAndCountry[1]]
+        if !set1.contains(set) {
+            print("NO")
+            return
+        }
+    }
+    
+    print("YES")
+}
