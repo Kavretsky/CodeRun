3fcdd64a2bbaa8fdaa9dcc0db3af5a054814b12a
Nikolay Kavretsky <kavretsky@gmail.com>
2024-08-29 16:40:53 +0300

    solve 3 task

---
+++ CodeRun/easy/3.LuckyBox.swift
@@ -0,0 +1,28 @@
+//
+//  LuckyTicket.swift
+//  CodeRun
+//
+//  Created by Nikolay Kavretsky on 29.08.2024.
+//
+
+import Foundation
+
+// 3 Суеверный коллекционер
+
+func firstLuckyBox() {
+    var number = Int(readLine()!)!
+    
+    while number < 1000000 {
+        if isLucky(number) {
+            print(number)
+            return
+        }
+        number += 1
+    }
+    
+}
+
+func isLucky(_ num: Int) -> Bool {
+    var arr = String(num).map { Int(String($0))! }
+    return arr[0] + arr[1] + arr[2] == arr[3] + arr[4] + arr[5]
+}
