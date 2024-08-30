3ccb12fc7b6f9fdd52e19714fd86cd714eab3aa8
Nikolay Kavretsky <kavretsky@gmail.com>
2024-08-30 15:51:17 +0300

    solve task 9

--- CodeRun/medium/9. RestoreReport.swift
+++ CodeRun/medium/9. RestoreReport.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+// 9.Восстановление отчётов
+
+func restoreRepost() {
+    let input1 = readLine()!.split(separator: " ").map { Int(String($0))! }
+    let pagesCount = input1[0]
+    let reportCount = input1[1]
+    var pages: Set<String> = Set(readLine()!.split(separator: " ").map { String($0) })
+    var dict = [String: [String]]()
+    let main = readLine()!
+    for _ in 1...reportCount {
+        let line = readLine()!.split(separator: " ").map { String($0) }
+        dict[line[0], default: []].append(line[1])
+    }
+    var visited = Set<String>()
+    var stack = [String]()
+    stack.append(main)
+    while !stack.isEmpty {
+        let currentPage = stack.removeLast()
+        if visited.contains(currentPage) {
+            continue
+        }
+        visited.insert(currentPage)
+        if pages.contains(currentPage) {
+            pages.remove(currentPage)
+        }
+        if let nextPages = dict[currentPage] {
+            stack.append(contentsOf: nextPages)
+        }
+        
+    }
+    
+    print(pages.count)
+}
