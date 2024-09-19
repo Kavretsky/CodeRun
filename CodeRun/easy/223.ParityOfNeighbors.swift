bdf6219556e400a04d9afd01b3ca13a6fe50b78b
Nikolay Kavretsky <kavretsky@gmail.com>
2024-09-19 22:45:17 +0300

    try to solve 223 =/

--- CodeRun/easy/223.ParityOfNeighbors.swift
+++ CodeRun/easy/223.ParityOfNeighbors.swift
@@ -19,106 +19,108 @@ func parityOfNeighbors() {
         print(nums.first!)
         return
     }
-    let numDic = Dictionary(grouping: nums) { element in
-        oddOrEven(element)
+    var odds = [Int]()
+    var evens = [Int]()
+    
+    for num in nums {
+        if isOdd(num) {
+            odds.append(num)
+        } else {
+            evens.append(num)
+        }
     }
-    guard abs(numDic["Odd", default: []].count - numDic["Even", default: []].count) <= 1
+    
+    var swapsCount = 0
+    var swappedIndices: [Int] = []
+     
+    guard abs(odds.count - evens.count) <= 1
     else {
         print("-1")
         return
     }
-    let mode: String
-    if numDic["Odd", default: []].count - numDic["Even", default: []].count == 0 {
-        mode = "0"
-    } else if numDic["Odd", default: []].count > numDic["Even", default: []].count {
-        mode = "Odd"
-    } else {
-        mode = "Even"
+    
+    func constructArr(_ arr1: [Int], arr2: [Int]) -> [Int] {
+        var result = [Int]()
+        var i = 0
+        var j = 0
+        var isI = true
+        for index in 0..<nums.count {
+            if isI {
+                result.append(arr1[i])
+                i += 1
+            } else {
+                result.append(arr2[j])
+                j += 1
+            }
+            isI.toggle()
+        }
+        return result
     }
     
-    var resultArr = [Int]()
-    
-    if mode == "0" {
-        var firstArr = numDic.first(where: { $0.value.contains(where: { $0 == nums.first! })})!.value
-        var secondArr = numDic.first(where: { !$0.value.contains(where: { $0 == nums.first! })})!.value
-        while !firstArr.isEmpty {
-            let firstNum = firstArr.first!
-            if nums[resultArr.count] != firstNum, let rigthIndex = firstArr.firstIndex(of: nums[resultArr.count]) {
-                resultArr.append(firstArr.remove(at: rigthIndex))
-            } else {
-                resultArr.append(firstArr.removeFirst())
-            }
-            
-            let secondNum = secondArr.first!
-            if nums[resultArr.count] != secondNum, let rigthIndex = secondArr.firstIndex(of: nums[resultArr.count]) {
-                resultArr.append(secondArr.remove(at: rigthIndex))
-            } else {
-                resultArr.append(secondArr.removeFirst())
-            }
-        }
-    } else if mode == "Even" {
-        var firstArr = numDic["Even", default: []]
-        var secondArr = numDic["Odd", default: []]
-        while firstArr.count > 1 {
-            let firstNum = firstArr.first!
-            if nums[resultArr.count] != firstNum, let rigthIndex = firstArr.firstIndex(of: nums[resultArr.count]) {
-                resultArr.append(firstArr.remove(at: rigthIndex))
-            } else {
-                resultArr.append(firstArr.removeFirst())
-            }
-            
-            let secondNum = secondArr.first!
-            if nums[resultArr.count] != secondNum, let rigthIndex = secondArr.firstIndex(of: nums[resultArr.count]) {
-                resultArr.append(secondArr.remove(at: rigthIndex))
-            } else {
-                resultArr.append(secondArr.removeFirst())
-            }
-        }
-        resultArr.append(firstArr.first!)
-    } else {
-        var firstArr = numDic["Odd", default: []]
-        var secondArr = numDic["Even", default: []]
-        while firstArr.count > 1 {
-            let firstNum = firstArr.first!
-            if nums[resultArr.count] != firstNum, let rigthIndex = firstArr.firstIndex(of: nums[resultArr.count]) {
-                resultArr.append(firstArr.remove(at: rigthIndex))
-            } else {
-                resultArr.append(firstArr.removeFirst())
-            }
-            
-            let secondNum = secondArr.first!
-            if nums[resultArr.count] != secondNum, let rigthIndex = secondArr.firstIndex(of: nums[resultArr.count]) {
-                resultArr.append(secondArr.remove(at: rigthIndex))
-            } else {
-                resultArr.append(secondArr.removeFirst())
-            }
-        }
-        resultArr.append(firstArr.first!)
-    }
-    var differents = 0
-    var differenceIndices = [Int]()
-    for index in 0..<nums.count {
-        if nums[index] != resultArr[index] {
-            differents += 1
-            differenceIndices.append(index)
-        }
-    }
-    
-    for i in 0..<resultArr.count - 1 {
-        for j in i + 1..<resultArr.count {
-            if resultArr[i] % 2 == resultArr[j] % 2, resultArr[i] != resultArr[j] {
-                if nums[i] == resultArr[j] || nums[j] == resultArr[i] {
-                    resultArr.swapAt(i, j)
-                    differents -= 1
+    func searchSwaps(for arr: [Int]) -> (Int, [Int]) {
+        var count = 0
+        var indices: [Int] = []
+        var skippedOdd = [Int]()
+        var skippedEven: [Int] = []
+        
+        for index in 0..<arr.count {
+            switch (isOdd(arr[index]), isOdd(nums[index])) {
+            case (true, true):
+                indices.append(nums[index])
+                if nums[index] != arr[index] {
+                    skippedOdd.append(arr[index])
+                }
+            case (false, false):
+                indices.append(nums[index])
+                if nums[index] != arr[index] {
+                    skippedEven.append(arr[index])
+                }
+            case (true, false):
+                count += 1
+                if skippedOdd.isEmpty {
+                    indices.append(arr[index])
+                } else {
+                    indices.append(skippedOdd.removeLast())
+                }
+            case (false, true):
+                count += 1
+                if skippedEven.isEmpty {
+                    indices.append(arr[index])
+                } else {
+                    indices.append(skippedEven.removeLast())
                 }
             }
         }
+        return (count, indices)
     }
     
-    print(differents)
-    print(resultArr.map { String($0)}.joined(separator: " "))
+    if odds.count > evens.count {
+        let result = constructArr(odds, arr2: evens)
+        (swapsCount, swappedIndices) = searchSwaps(for: result)
+        
+    } else if odds.count < evens.count {
+        let result = constructArr(evens, arr2: odds)
+        (swapsCount, swappedIndices) = searchSwaps(for: result)
+        
+    } else {
+        let arr1 = constructArr(odds, arr2: evens)
+        let (swapsRes, res) = searchSwaps(for: arr1)
+        let arr2 = constructArr(evens, arr2: odds)
+        let (swapsRes2, res2) = searchSwaps(for: arr2)
+        if swapsRes < swapsRes2 {
+            swapsCount = swapsRes
+            swappedIndices = res
+        } else {
+            swapsCount = swapsRes2
+            swappedIndices = res2
+        }
+    }
+    
+    print(swapsCount)
+    print(swappedIndices.map { String($0)}.joined(separator: " "))
+    
 }
 
-func oddOrEven(_ num: Int) -> String {
-    num % 2 == 1 ? "Odd" : "Even"
+func isOdd(_ num: Int) -> Bool {
+    num % 2 == 1
 }
